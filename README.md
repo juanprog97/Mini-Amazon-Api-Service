e76396a4c28ce9486b36adbe4263f421cd46e78c
juan <juancard190@gmail.com>
2019-07-14 22:00:58 -0500

    Create DB and Add Data

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Mini-Amazon-Api-Service
+https://hub.docker.com/_/neo4j/
+https://marcobonzanini.com/2015/04/06/getting-started-with-neo4j-and-python/
+
+1. Create a server
+sudo docker run --name neo4j -p 7474:7474 -p 7687:7687 --volume=$HOME/neo4j/data:/data -d neo4j <br />
+or <br />
+sudo docker run --name neo4j -p 7474:7474 -p 7687:7687 -d neo4j
+
+2. Verify the database in your localhost: http://localhost:7474/ <br />
+  Note: neo4j is the current password, change it! and remember it!
+
+3. In your localmachine install: <br />
+sudo pip install neo4jrestclient
+
+4. Open another terminal and execute the code createDB.py NOTE: change the password!
+
+5. View in http://localhost:7474/  Database Information click in * (asterisk) <br />
+The Neo4j Browser available at http://localhost:7474/ provides a nice way to query the DB and visualise the results, both as a list of record and in a visual form.
---
+++ createDB.py
@@ -0,0 +1,61 @@
+from neo4jrestclient.client import GraphDatabase
+db = GraphDatabase("http://localhost:7474", username="neo4j", password="admin")
+
+def main():
+
+    #Add User
+    print("\nAddUser\n")
+    data = open("./Datos/usr.txt", "r")
+    node = db.labels.create("User")
+    cont= 1
+    data.readline()
+    users = data.readline().strip().split(" ")
+    while(len(users) != 1):
+        datUser,datPass,datActive = users[0],users[1],users[2]
+        userNew = db.nodes.create(name=datUser,password=datPass,active=datActive,id=cont)
+        users = data.readline().strip().split(" ")
+        print("*ADD, User:{0}, Password:{1}, Active:{2}".format(datUser,datPass,datActive))
+        node.add(userNew)
+        cont+=1
+
+    #CreateDepartaments
+    print("\nCreate Departaments\n")
+    data = open("./Datos/departamentos.txt", "r")
+    node = db.labels.create("Department")
+    nodeCat = db.labels.create("Category")
+    nodeSub = db.labels.create("SubCategory")
+    cont = 1
+    data.readline()
+    listDepat = ["Book","Coche y Moto","Computadores","Digital Music","Electronics","Industrial&Scientific","Kitchen&Home"
+                 "Luggage","Movies&TV","Music,CDs & Vinyl","SportAndOutdoors","ToysAndGames"
+                ]
+    depar = data.readline().strip().split("|")
+    while(len(depar) != 1):
+        datId, datName,datActive,dataCreateDate,datCountry = depar[0],depar[1],depar[2],depar[3],depar[4]
+        depaNew = db.nodes.create(name=datName,id=datId,active=datActive,create_date=dataCreateDate,country=datCountry)
+        depar = data.readline().strip().split("|")
+        print("*Add,id:{0}, Departaments:{1},Active:{2},CreateDate:{3},Country:{4}".format(datId,datName,datActive,dataCreateDate,datCountry))
+        node.add(depaNew)
+        if(datActive != '0'):
+            DataDep = open(f"./Datos/{datName}.txt", "r")
+            DataDep.readline()
+            firstLe =""
+            while(True):
+                 dat = DataDep.readline().strip().split("|")
+                 if(len(dat) == 1):
+                     break
+                 else:
+                     if(firstLe != dat[2]):
+                         firstLe = dat[2]
+                         cate = db.nodes.create(name=firstLe)
+                         nodeCat.add(cate)
+                         cate.relationships.create("Category",depaNew)
+                         print("\n*Add,Category:{0}-----\n".format(firstLe))
+
+                     else:
+                        subCa = db.nodes.create(name=dat[3],active=dat[4],createDate=dat[5],country=dat[6])
+                        nodeSub.add(subCa)
+                        subCa.relationships.create("SubCategory",cate)
+                        print("*Add,SubCategory:{0},Active:{1},Create Date:{2},Country:{3}".format(dat[3],dat[4],dat[5],dat[6]))
+
+main()
